ef9f7f9b807f6c7bcebc261bb91da3142b70d429
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-04-03 22:35:52 +0200

    Added the new "brand_color" design option

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -208,6 +208,12 @@ class Configuration implements ConfigurationInterface
                             ->end()
                         ->end()
 
+                        ->scalarNode('brand_color')
+                            ->info('The color used in the backend design to highlight important elements.')
+                            ->defaultValue('#D47843')
+                            ->treatNullLike('#D47843')
+                        ->end()
+
                         ->variableNode('form_theme')
                             ->defaultValue(array('@EasyAdmin/form/bootstrap_3_horizontal_layout.html.twig'))
                             ->treatNullLike(array('@EasyAdmin/form/bootstrap_3_horizontal_layout.html.twig'))

8be2677b69af1405d119266f695e67517e47d446
Alex Rock Ancelet <pierstoval@gmail.com>
2015-05-18 11:58:08 +0200

    Base setup for showing better errors in admin

---
+++ Event/ExceptionListener.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the EasyAdminBundle.
+ *
+ * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JavierEguiluz\Bundle\EasyAdminBundle\Event;
+
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ExceptionListener
+{
+
+    /**
+     * @var TwigEngine
+     */
+    protected $templating;
+
+    public function __construct(TwigEngine $templating)
+    {
+        $this->templating = $templating;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $controller = $event->getRequest()->attributes->get('_controller');
+
+        if (strpos($controller, 'JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController') !== false) {
+            $e = $event->getException();
+            $response = new Response('', 500);
+
+            $exceptions = array();
+
+            do {
+                $exceptions[] = array(
+                    'class'         => get_class($e),
+                    'file'          => $e->getFile(),
+                    'line'          => $e->getLine(),
+                    'code'          => $e->getCode(),
+                    'message'       => $e->getMessage(),
+                    'trace'         => $e->getTrace(),
+                    'traceAsString' => $e->getTraceAsString(),
+                );
+            } while ($e = $e->getPrevious());
+
+            $response->setContent($this->templating->render('@EasyAdmin/error/exception.html.twig', array('exceptions' => $exceptions)));
+
+            $event->setResponse($response);
+        }
+    }
+
+}

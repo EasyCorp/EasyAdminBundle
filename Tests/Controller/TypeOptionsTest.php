2006e365a0f2e13602966b74ce97efd944e1412c
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-10-03 20:37:47 +0200

    Added tests for the new "type_options" option

---
+++ Tests/Controller/TypeOptionsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the EasyAdminBundle.
+ *
+ * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JavierEguiluz\Bundle\EasyAdminBundle\Tests\Controller;
+
+use Symfony\Component\DomCrawler\Crawler;
+use JavierEguiluz\Bundle\EasyAdminBundle\Tests\Fixtures\AbstractTestCase;
+
+class TypeOptionsTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->initClient(array('environment' => 'type_options'));
+    }
+
+    public function testNewViewTypeOptions()
+    {
+        $crawler = $this->requestNewView();
+
+        $this->assertEquals('readonly', $crawler->filter('#main form #form_name')->attr('readonly'));
+
+        $this->assertCount(201, $crawler->filter('#main form #form_parent input[type=radio]'));
+        $this->assertEquals('Select a category...', $crawler->filter('#main form #form_parent div.radio')->eq(0)->text());
+    }
+
+    public function testEditViewTypeOptions()
+    {
+        $crawler = $this->requestEditView();
+
+        $this->assertContains('col-sm-6', $crawler->filter('#main form label[for=form_name]')->attr('class'));
+        $this->assertContains('col-sm-6', $crawler->filter('#main form input#form_name')->attr('class'));
+
+        $this->assertContains('col-sm-4', $crawler->filter('#main form label[for=form_parent]')->attr('class'));
+        $this->assertContains('col-sm-10', $crawler->filter('#main form select#form_parent')->attr('class'));
+    }
+
+    /**
+     * @return Crawler
+     */
+    private function requestNewView()
+    {
+        return $this->getBackendPage(array(
+            'action' => 'new',
+            'entity' => 'Category',
+        ));
+    }
+
+    /**
+     * @return Crawler
+     */
+    private function requestEditView()
+    {
+        return $this->getBackendPage(array(
+            'action' => 'edit',
+            'entity' => 'Category',
+            'id' => '50',
+        ));
+    }
+}

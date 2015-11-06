d68ead8b5d3eb678175de88597ac1e33e6b48f1c
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-11-06 23:09:20 +0100

    Removed duplicated "use" imports

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -17,8 +17,6 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\EventListener\ExceptionListener as BaseExceptionListener;
 use Symfony\Component\HttpKernel\Exception\HttpException;

35a37d019080b2561abba8e1449c139129c89ec8
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-11-28 11:43:55 +0100

    Allow to set date/time form fields to null

--- Resources/public/javascript/admin.js
+++ Resources/public/javascript/admin.js
@@ -9,6 +9,7 @@ $(function () {
 
     mainMenuDropShadow.apply(mainMenu);
     mainMenuResponsiveCollapse();
+    createNullableControls();
 });
 
 function mainMenuDropShadow() {
@@ -31,4 +32,23 @@ function mainMenuResponsiveCollapse() {
     } else {
         mainMenuItems.flexMenu({ 'undo': true });
     }
+}
+
+function createNullableControls() {
+    var fnNullDates = function() {
+        var checkbox = $(this);
+
+        checkbox.closest('.form-group').find('select').each(function() {
+            var formFieldIsDisabled = checkbox.is(':checked');
+            $(this).prop('disabled', formFieldIsDisabled);
+
+            if (formFieldIsDisabled) {
+                $(this).parent().slideUp({ duration: 200 });
+            } else {
+                $(this).parent().slideDown({ duration: 200 });
+            }
+        });
+    };
+
+    $('.nullable-control :checkbox').bind('change', fnNullDates).each(fnNullDates);
 }

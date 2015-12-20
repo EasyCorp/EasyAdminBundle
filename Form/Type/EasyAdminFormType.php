10e74564a365984f7777dbda0ac103de9ecfd99b
Maxime Steinhausser <maxime.steinhausser@gmail.com>
2015-12-20 12:15:10 +0100

    Fix CKEditor support when "config_name" option is defined

--- Form/Type/EasyAdminFormType.php
+++ Form/Type/EasyAdminFormType.php
@@ -97,9 +97,9 @@ class EasyAdminFormType extends AbstractType
             }
 
             // when using a WYSIWYG CKEditor without custom config, apply a better default config
-            if (class_exists('\Ivory\CKEditorBundle\Form\Type\CKEditorType')) {
-                $isCkeditorField = in_array($metadata['fieldType'], array('ckeditor', 'Ivory\CKEditorBundle\Form\Type\CKEditorType'));
-                if ($isCkeditorField && !isset($metadata['type_options']['config']['toolbar'])) {
+            if (class_exists('\\Ivory\\CKEditorBundle\\Form\\Type\\CKEditorType')) {
+                $isCkeditorField = in_array($metadata['fieldType'], array('ckeditor', 'Ivory\\CKEditorBundle\\Form\\Type\\CKEditorType'));
+                if ($isCkeditorField && !isset($metadata['type_options']['config']['toolbar']) && !isset($metadata['type_options']['config_name'])) {
                     $formFieldOptions['config']['toolbar'] = array(
                         array('name' => 'styles', 'items' => array('Bold', 'Italic', 'Strike', 'Link')),
                         array('name' => 'lists', 'items' => array('BulletedList', 'NumberedList', '-','Outdent','Indent')),

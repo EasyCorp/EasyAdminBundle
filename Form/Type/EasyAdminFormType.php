11ca06c6e96a66a0ee37f1849d891cf2dbf0f5c7
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-01-03 12:39:46 +0100

    Minor code refactor for the previous merge

--- Form/Type/EasyAdminFormType.php
+++ Form/Type/EasyAdminFormType.php
@@ -129,9 +129,10 @@ class EasyAdminFormType extends AbstractType
     private function getAttributesNormalizer()
     {
         return function (Options $options, $value) {
-            return array_replace(array(
-                'id' => $options['view'].'-form',
-            ), $value);
+            return array_replace(
+                array('id' => $options['view'].'-form'),
+                $value
+            );
         };
     }
 

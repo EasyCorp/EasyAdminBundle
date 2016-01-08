79e4dd77bb557e419188226c743223ed9651b502
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-01-08 20:51:52 +0100

    Improved tests

--- Tests/Controller/SplitConfigurationTest.php
+++ Tests/Controller/SplitConfigurationTest.php
@@ -16,18 +16,26 @@ use JavierEguiluz\Bundle\EasyAdminBundle\Tests\Fixtures\AbstractTestCase;
 
 class SplitConfigurationTest extends AbstractTestCase
 {
-    public function setUp()
+    public function testConfigurationInDifferentFiles()
     {
-        parent::setUp();
-
         $this->initClient(array('environment' => 'split_configuration'));
-    }
-
-    public function testConfiguredEntities()
-    {
         $backendConfig = $this->client->getContainer()->getParameter('easyadmin.config');
 
         $this->assertEquals(array('Category', 'Product'), array_keys($backendConfig['entities']));
+
         $this->assertEquals('Categories', $backendConfig['entities']['Category']['label']);
+
+        $this->assertEquals('Second Site Name', $backendConfig['site_name']);
+        $this->assertEquals('blue', $backendConfig['design']['brand_color']);
+    }
+
+    public function testConfigurationErrorsInDifferentFiles()
+    {
+        $this->setExpectedException(
+            'Symfony\Component\Config\Definition\Exception\InvalidConfigurationException',
+            'The value "wrong_value" is not allowed for path "easy_admin.design.color_scheme". Permissible values: "dark", "light"'
+        );
+
+        $this->initClient(array('environment' => 'split_configuration_error'));
     }
 }

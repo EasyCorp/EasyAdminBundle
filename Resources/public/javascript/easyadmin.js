774c22389ff794679a151a12b41c760aa316f1f8
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-01-20 20:09:59 +0100

    Made the navigation menu sticky

--- Resources/public/javascript/easyadmin.js
+++ Resources/public/javascript/easyadmin.js
@@ -2,8 +2,8 @@ $(function () {
     var body = $('body');
 
     body
-        .on('expanded.pushMenu', navigationToggled(false))
-        .on('collapsed.pushMenu', navigationToggled(true))
+        .on('expanded.pushMenu', toggleNavigation(false))
+        .on('collapsed.pushMenu', toggleNavigation(true))
     ;
 
     createNullableControls();
@@ -28,15 +28,45 @@ function createNullableControls() {
     $('.nullable-control :checkbox').bind('change', fnNullDates).each(fnNullDates);
 }
 
-function navigationToggled(hidden) {
-    var key = '_easyadmin_navbar_hidden';
+function toggleNavigation(hidden) {
+    var cookieName = '_easyadmin_navigation_collapse';
 
     return function() {
         if (hidden) {
-            docCookies.setItem(key, true, Infinity);
+            createCookie(cookieName, true, Infinity);
         } else {
-            docCookies.removeItem(key);
+            deleteCookie(cookieName);
         }
     };
 }
 
+function createCookie(name, value, expiration, path, domain, secure)
+{
+    if (!name || /^(?:expires|max\-age|path|domain|secure)$/i.test(name)) { return false; }
+
+    var expires = "";
+    if (expiration) {
+        switch (expiration.constructor) {
+            case Number:
+                expires = expiration === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + expiration;
+                break;
+            case String:
+                expires = "; expires=" + expiration;
+                break;
+            case Date:
+                expires = "; expires=" + expiration.toUTCString();
+                break;
+        }
+    }
+
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "") + (secure ? "; secure" : "");
+
+    return true;
+}
+
+function deleteCookie(name, path, domain)
+{
+    document.cookie = encodeURIComponent(name) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "");
+
+    return true;
+}

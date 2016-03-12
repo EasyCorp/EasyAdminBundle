845502036587862663ec07d2796a078810f0cde9
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-03-12 12:35:24 +0100

    Removed some unneeded code now that fixtures are reset befor each test

--- Tests/Controller/DefaultBackendTest.php
+++ Tests/Controller/DefaultBackendTest.php
@@ -444,12 +444,6 @@ class DefaultBackendTest extends AbstractTestCase
         $crawler = $this->client->submit($form);
 
         $this->assertContains($categoryName, $crawler->filter('#main table tr')->eq(1)->text(), 'The newly created category is displayed in the first data row of the "list" table.');
-
-        // remove the new category to avoid errors in other tests
-        $doctrine = $this->client->getContainer()->get('doctrine');
-        $category = $doctrine->getRepository('AppTestBundle:Category')->findOneByName($categoryName);
-        $doctrine->getManager()->remove($category);
-        $doctrine->getManager()->flush();
     }
 
     public function testSearchViewTitle()

2375c737f5b0125d6a84c4d12b81b9a08db2ce7a
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-04-03 11:33:36 +0200

    Removed unused imports

--- Form/Type/EasyAdminAutocompleteType.php
+++ Form/Type/EasyAdminAutocompleteType.php
@@ -3,9 +3,6 @@
 namespace JavierEguiluz\Bundle\EasyAdminBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class EasyAdminAutocompleteType extends AbstractType

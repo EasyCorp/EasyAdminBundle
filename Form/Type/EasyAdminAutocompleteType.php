528746d6945cea3dd1a31aec2bc942d5df90a9e9
Yonel Ceruto <yonelceruto@gmail.com>
2016-04-04 15:56:36 -0400

    fixed initially no value is displayed and invalid value on submit the form

--- Form/Type/EasyAdminAutocompleteType.php
+++ Form/Type/EasyAdminAutocompleteType.php
@@ -2,19 +2,76 @@
 
 namespace JavierEguiluz\Bundle\EasyAdminBundle\Form\Type;
 
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+/**
+ * Autocomplete form type.
+ *
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ * @author Yonel Ceruto <yonelceruto@gmail.com>
+ */
 class EasyAdminAutocompleteType extends AbstractType
 {
+    private $preSetData = false;
+    private $preSubmit = false;
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (!$this->preSetData) {
+            // avoid infinite call to PRE_SET_DATA event
+            $this->preSetData = true;
+
+            $preSetDataListener = function (FormEvent $event) use ($options) {
+                $form = $event->getForm();
+                $data = $event->getData();
+                // settings selected data
+                $options['choices'] = is_array($data) || $data instanceof Collection ? $data : array($data);
+                // redefine form and choices option
+                $form->getParent()->add($form->getName(), 'JavierEguiluz\Bundle\EasyAdminBundle\Form\Type\EasyAdminAutocompleteType', $options);
+            };
+
+            $builder->addEventListener(FormEvents::PRE_SET_DATA, $preSetDataListener);
+        }
+
+        if (!$this->preSubmit) {
+            $self = $this;
+            $preSubmitListener = function (FormEvent $event) use ($self, $options) {
+                // avoid infinite call to PRE_SUBMIT event
+                $self->preSubmit = true;
+
+                $form = $event->getForm();
+                $data = $event->getData();
+                // normalize data choices
+                $normData = $options['em']->getRepository($options['class'])->findBy(array(
+                    $options['id_reader']->getIdField() => $data
+                ));
+                // settings selected data
+                $options['choices'] = $normData;
+
+                // redefine form and choices option
+                $form->getParent()->add($form->getName(), 'JavierEguiluz\Bundle\EasyAdminBundle\Form\Type\EasyAdminAutocompleteType', $options);
+                // submit data to new form
+                $form->getParent()->get($form->getName())->submit($data);
+            };
+
+            $builder->addEventListener(FormEvents::PRE_SUBMIT, $preSubmitListener);
+        }
+    }
+
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setRequired(array(
-            'class'
+        $resolver->setDefaults(array(
+            // this prevents the form field to load all the entity records from the database
+            'choices' => array(),
         ));
     }
 
@@ -40,7 +97,7 @@ class EasyAdminAutocompleteType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getName()
+    public function getBlockPrefix()
     {
         return 'easyadmin_autocomplete';
     }
@@ -48,8 +105,8 @@ class EasyAdminAutocompleteType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
+    public function getName()
     {
-        return $this->getName();
+        $this->getBlockPrefix();
     }
 }

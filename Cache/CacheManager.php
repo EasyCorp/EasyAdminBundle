d489a32186c3eb1be9ee0797ff1c03974619d5c6
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-04-27 21:45:42 +0200

    Added PHPdoc info

--- Cache/CacheManager.php
+++ Cache/CacheManager.php
@@ -14,7 +14,9 @@ namespace JavierEguiluz\Bundle\EasyAdminBundle\Cache;
 use Doctrine\Common\Cache\FilesystemCache;
 
 /**
- * ...
+ * It provides a file system based cache exposing methods with the same names
+ * as in the PSR-6 Cache standard. This will simplify the eventual replacement
+ * of Doctrine Cache by Symfony Cache.
  */
 class CacheManager extends FilesystemCache
 {
@@ -23,16 +25,32 @@ class CacheManager extends FilesystemCache
         parent::__construct($cacheDir);
     }
 
+    /**
+     * @param  string $key
+     *
+     * @return mixed
+     */
     public function getItem($key)
     {
         return parent::fetch($key);
     }
 
+    /**
+     * @param  string $key
+     *
+     * @return bool
+     */
     public function hasItem($key)
     {
         return parent::contains($key);
     }
 
+    /**
+     * @param  string $key
+     * @param  mixed $item
+     *
+     * @return bool
+     */
     public function save($key, $item)
     {
         parent::save($key, $item);

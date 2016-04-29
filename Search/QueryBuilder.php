82cf46df81148ce2ddb4a4e1043df7eeba0ca530
vrobic <vrobic@umanit.fr>
2016-04-29 15:31:36 +0200

    Ajax autocomplete for related entities is now case insensitive

--- Search/QueryBuilder.php
+++ Search/QueryBuilder.php
@@ -82,14 +82,16 @@ class QueryBuilder
             $isTextField = in_array($metadata['dataType'], array('string', 'text', 'guid'));
 
             if ($isNumericField && is_numeric($searchQuery)) {
-                $queryBuilder->orWhere(sprintf('entity.%s = :exact_query', $name));
+                $queryBuilder->orWhere(sprintf('LOWER(entity.%s) = :exact_query', $name));
                 // adding '0' turns the string into a numeric value
                 $queryParameters['exact_query'] = 0 + $searchQuery;
             } elseif ($isTextField) {
-                $queryBuilder->orWhere(sprintf('entity.%s LIKE :fuzzy_query', $name));
+                $searchQuery = strtolower($searchQuery);
+                
+                $queryBuilder->orWhere(sprintf('LOWER(entity.%s) LIKE :fuzzy_query', $name));
                 $queryParameters['fuzzy_query'] = '%'.$searchQuery.'%';
 
-                $queryBuilder->orWhere(sprintf('entity.%s IN (:words_query)', $name));
+                $queryBuilder->orWhere(sprintf('LOWER(entity.%s) IN (:words_query)', $name));
                 $queryParameters['words_query'] = explode(' ', $searchQuery);
             }
         }

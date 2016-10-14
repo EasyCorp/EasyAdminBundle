d3c41137fac4243e02df410fe4fd592d472d31fe
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-10-14 17:09:53 +0200

    Prevent error messages when installing an app and the DB doesn't exist

--- Cache/ConfigWarmer.php
+++ Cache/ConfigWarmer.php
@@ -29,8 +29,14 @@ class ConfigWarmer implements CacheWarmerInterface
 
     public function warmUp($cacheDir)
     {
-        // this forces the full processing of the backend configuration
-        $this->configManager->getBackendConfig();
+        try {
+            // this forces the full processing of the backend configuration
+            $this->configManager->getBackendConfig();
+        } catch (\PDOException $e) {
+            // this occurs for example when the database doesn't exist yet and the
+            // project is being installed ('composer install' clears the cache at the end)
+            // ignore this error at this point and display an error message later
+        }
     }
 
     public function isOptional()

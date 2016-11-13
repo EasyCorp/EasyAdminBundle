f906f893b401463de87dada0e62f135a4a56979f
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-11-13 17:16:17 +0100

    Prepared the 1.16.0 release

--- EasyAdminBundle.php
+++ EasyAdminBundle.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    const VERSION = '1.15.4-DEV';
+    const VERSION = '1.16.0';
 
     public function build(ContainerBuilder $container)
     {

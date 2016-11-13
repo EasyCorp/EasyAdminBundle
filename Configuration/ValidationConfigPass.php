7bbcdf690f234432caab79d0edde145ddac7588d
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-11-13 13:50:56 +0100

    Improved the __toString() check

--- Configuration/ValidationConfigPass.php
+++ Configuration/ValidationConfigPass.php
@@ -37,11 +37,23 @@ class ValidationConfigPass implements ConfigPassInterface
     private function processAssociationsConfig(array $backendConfig)
     {
         foreach ($backendConfig['entities'] as $entityName => $entityConfig) {
-            foreach (array('edit', 'list', 'new', 'search', 'show') as $view) {
+            foreach (array('list', 'search', 'show') as $view) {
                 foreach ($entityConfig[$view]['fields'] as $fieldName => $fieldConfig) {
-                    if ('association' === $fieldConfig['dataType']) {
+                    // Doctrine associations that don't define a custom template must define a __toString() method
+                    if ('association' === $fieldConfig['dataType'] && 0 === strpos($fieldConfig['template'], '@EasyAdmin/default/')) {
                         if (!method_exists($fieldConfig['targetEntity'], '__toString')) {
-                            throw new \InvalidArgumentException(sprintf('The "%s" class must define a "__toString()" method because it is used in the "%s" field of the "%s" view in the "%s" entity.', $fieldConfig['targetEntity'], $fieldName, $view, $entityName));
+                            throw new \InvalidArgumentException(sprintf('The "%s" class must define a "__toString()" method because it is used as the "%s" field in the "%s" view of the "%s" entity.', $fieldConfig['targetEntity'], $fieldName, $view, $entityName));
+                        }
+                    }
+                }
+            }
+
+            foreach (array('edit', 'new') as $view) {
+                foreach ($entityConfig[$view]['fields'] as $fieldName => $fieldConfig) {
+                    // fields used in autocomplete form types must define a __toString() method
+                    if ('easyadmin_autocomplete' === $fieldConfig['fieldType']) {
+                        if (!method_exists($fieldConfig['targetEntity'], '__toString')) {
+                            throw new \InvalidArgumentException(sprintf('The "%s" class must define a "__toString()" method because it is used as the "%s" autocomplete field in the "%s" view of the "%s" entity.', $fieldConfig['targetEntity'], $fieldName, $view, $entityName));
                         }
                     }
                 }

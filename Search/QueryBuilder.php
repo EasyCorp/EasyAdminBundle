c742b8dd7ce123af299403bee95668378e816211
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-12-17 12:20:56 +0100

    Fixed a syntax error

--- Search/QueryBuilder.php
+++ Search/QueryBuilder.php
@@ -86,7 +86,7 @@ class QueryBuilder
         foreach ($entityConfig['search']['fields'] as $name => $metadata) {
             $isNumericField = in_array($metadata['dataType'], array('integer', 'number', 'smallint', 'bigint', 'decimal', 'float'));
             $isTextField = in_array($metadata['dataType'], array('string', 'text', 'guid'));
-            $isGuidField = 'guid' === metadata['dataType'];
+            $isGuidField = 'guid' === $metadata['dataType'];
 
             if ($isNumericField && is_numeric($searchQuery)) {
                 $queryBuilder->orWhere(sprintf('entity.%s = :exact_query', $name));

533da093ba4331040c10771a6e7c010daa640364
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-04-24 15:52:03 +0200

    Fixed some search conditions related to integers

--- Search/QueryBuilder.php
+++ Search/QueryBuilder.php
@@ -95,8 +95,8 @@ class QueryBuilder
         }
 
         $isSearchQueryNumeric = is_numeric($searchQuery);
-        $isSearchQuerySmallInteger = is_int($searchQuery) && abs($searchQuery) <= 32767;
-        $isSearchQueryInteger = is_int($searchQuery) && abs($searchQuery) <= PHP_INT_MAX;
+        $isSearchQuerySmallInteger = (is_int($searchQuery) || ctype_digit($searchQuery)) && abs($searchQuery) <= 32767;
+        $isSearchQueryInteger = (is_int($searchQuery) || ctype_digit($searchQuery)) && abs($searchQuery) <= PHP_INT_MAX;
         $isSearchQueryUuid = 1 === preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $searchQuery);
         $lowerSearchQuery = mb_strtolower($searchQuery);
 

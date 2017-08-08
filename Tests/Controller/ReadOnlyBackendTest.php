465cf7a442b14495a3124b5817ce833d6fe175d5
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-08-08 13:42:46 +0200

    Removed unused imports

--- Tests/Controller/ReadOnlyBackendTest.php
+++ Tests/Controller/ReadOnlyBackendTest.php
@@ -12,7 +12,6 @@
 namespace JavierEguiluz\Bundle\EasyAdminBundle\Tests\Controller;
 
 use JavierEguiluz\Bundle\EasyAdminBundle\Tests\Fixtures\AbstractTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class ReadOnlyBackendTest extends AbstractTestCase
 {

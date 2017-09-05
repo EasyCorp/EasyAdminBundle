30186bb852629eaab90ccc960a23fcce1c49d79e
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-09-05 19:50:45 +0200

    Reorganized contents

--- legacy/EasyAdminBundle.php
+++ legacy/EasyAdminBundle.php
@@ -6,8 +6,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 @trigger_error('The '.__NAMESPACE__.'\EasyAdminBundle class is deprecated since version 1.16 and will be removed in 2.0. Use the EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle class instead.', E_USER_DEPRECATED);
 
-class_exists(EasyAdminBundle::class);
+class_exists('EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle');
 
-class EasyAdminBundle extends Bundle
-{
+if (\false) {
+    class EasyAdminBundle extends Bundle
+    {
+    }
 }

cce54bde0d485aedd1e6fe3686e49f10dcc0ff63
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-09-09 12:22:59 +0200

    Added the legacy class aliases

---
+++ legacy/Form/Type/Configurator/CheckboxTypeConfigurator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JavierEguiluz\Bundle\EasyAdminBundle\Form\Type\Configurator;
+
+@trigger_error('The '.__NAMESPACE__.'\CheckboxTypeConfigurator class is deprecated since version 1.16 and will be removed in 2.0. Use the EasyCorp\Bundle\EasyAdminBundle\Form\Type\Configurator\CheckboxTypeConfigurator class instead.', E_USER_DEPRECATED);
+
+class_exists('EasyCorp\Bundle\EasyAdminBundle\Form\Type\Configurator\CheckboxTypeConfigurator');
+
+if (\false) {
+    class CheckboxTypeConfigurator implements TypeConfiguratorInterface
+    {
+    }
+}

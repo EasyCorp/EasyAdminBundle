fa3b021001bd5ed935d47cfaa8a2ceba4db931fd
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-09-09 16:55:47 +0200

    Fixed a bunch of tests

--- tests/Controller/ReadOnlyBackendTest.php
+++ tests/Controller/ReadOnlyBackendTest.php
@@ -50,7 +50,7 @@ class ReadOnlyBackendTest extends AbstractTestCase
     {
         $this->requestEditView();
 
-        $this->assertSame(500, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
         $this->assertContains('The requested &quot;edit&quot; action is not allowed for the &quot;Category&quot; entity. Solution: remove the &quot;edit&quot; action from the &quot;disabled_actions&quot; option, which can be configured globally for the entire backend or locally for the &quot;Category&quot; entity.', $this->client->getResponse()->getContent());
     }
 
@@ -58,7 +58,7 @@ class ReadOnlyBackendTest extends AbstractTestCase
     {
         $this->requestNewView();
 
-        $this->assertSame(500, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
         $this->assertContains('The requested &quot;new&quot; action is not allowed for the &quot;Category&quot; entity. Solution: remove the &quot;new&quot; action from the &quot;disabled_actions&quot; option, which can be configured globally for the entire backend or locally for the &quot;Category&quot; entity.', $this->client->getResponse()->getContent());
     }
 }

e24adadf5308e42c42ee964be5ff1bd1ef32b442
Konstantin Grachev <me@grachevko.ru>
2017-09-18 18:34:01 +0300

    Set label and translation_domain to form, instead of manually render in twig.

---
+++ src/Form/Type/Configurator/TypeConfigurator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace EasyCorp\Bundle\EasyAdminBundle\Form\Type\Configurator;
+
+use EasyCorp\Bundle\EasyAdminBundle\Configuration\ConfigManager;
+use Symfony\Component\Form\FormConfigInterface;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class TypeConfigurator implements TypeConfiguratorInterface
+{
+    /**
+     * @var ConfigManager
+     */
+    private $configManager;
+
+    public function __construct(ConfigManager $configManager)
+    {
+        $this->configManager = $configManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure($name, array $options, array $metadata, FormConfigInterface $parentConfig)
+    {
+        if (!array_key_exists('label', $options) && array_key_exists('label', $metadata)) {
+            $options['label'] = $metadata['label'];
+        }
+
+        if (empty($options['translation_domain'])) {
+            $entityConfig = $this->configManager->getEntityConfig($options['entity']);
+
+            if (!empty($entityConfig['translation_domain'])) {
+                $options['translation_domain'] = $entityConfig['translation_domain'];
+            }
+        }
+
+        return $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($type, array $options, array $metadata)
+    {
+        return true;
+    }
+}

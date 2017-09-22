ac1c972a557e1a2dfd94cd549c661d061bf1bd64
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-09-22 19:46:58 +0200

    Added the copyright notice
    I'm thinking about removing all these notices in the code, but before that, let's add it here too to be consistent with the rest of the code

--- src/Form/Type/Configurator/TypeConfigurator.php
+++ src/Form/Type/Configurator/TypeConfigurator.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the EasyAdminBundle.
+ *
+ * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace EasyCorp\Bundle\EasyAdminBundle\Form\Type\Configurator;
 
 use EasyCorp\Bundle\EasyAdminBundle\Configuration\ConfigManager;

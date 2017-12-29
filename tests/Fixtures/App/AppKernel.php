a3f2458269fe69b4fb77ce1adf9fe3f2e2109954
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-12-29 18:12:15 +0100

    Add logout_on_user_change option conditionally

--- tests/Fixtures/App/AppKernel.php
+++ tests/Fixtures/App/AppKernel.php
@@ -53,6 +53,16 @@ class AppKernel extends Kernel
                 ));
             });
         }
+
+        if ($this->requiresLogoutOnUserChange()) {
+            $container->loadFromExtension('security', array(
+                'firewalls' => array(
+                    'main' => array(
+                        'logout_on_user_change' => true,
+                    ),
+                ),
+            ));
+        }
     }
 
     /**
@@ -79,5 +89,10 @@ class AppKernel extends Kernel
     protected function requiresTemplatingConfig()
     {
         return 2 === (int) Kernel::MAJOR_VERSION && 3 === (int) Kernel::MINOR_VERSION;
+    }
+
+    protected function requiresLogoutOnUserChange()
+    {
+        return (int) Kernel::VERSION_ID >= 30400;
     }
 }

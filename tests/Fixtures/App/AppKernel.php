045efa970986f47e8e84c2b4a10d214a2e32a76a
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-03-01 08:35:18 +0100

    Removed the copyright notice from all files

--- tests/Fixtures/App/AppKernel.php
+++ tests/Fixtures/App/AppKernel.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the EasyAdminBundle.
- *
- * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;

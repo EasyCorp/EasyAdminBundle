a114319e00144a06fb00a3432557e0ff49ea04ba
Konstantin Grachev <me@grachevko.ru>
2018-03-03 23:17:00 +0300

    Clear previous cache on warmUp

--- src/Configuration/CacheConfigManager.php
+++ src/Configuration/CacheConfigManager.php
@@ -3,12 +3,15 @@
 namespace EasyCorp\Bundle\EasyAdminBundle\Configuration;
 
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 
 /**
  * @author Konstantin Grachev <me@grachevko.ru>
  */
-final class CacheConfigManager implements ConfigManagerInterface
+final class CacheConfigManager implements ConfigManagerInterface, CacheWarmerInterface
 {
+    private const CACHE_KEY = 'easyadmin.processed_config';
+
     /**
      * @var ConfigManagerInterface
      */
@@ -40,7 +43,7 @@ final class CacheConfigManager implements ConfigManagerInterface
             return $this->configManager->getBackendConfig($propertyPath);
         }
 
-        $item = $this->cache->getItem('easyadmin.processed_config');
+        $item = $this->cache->getItem(self::CACHE_KEY);
 
         if (!$item->isHit()) {
             $item->set($this->configManager->getBackendConfig($propertyPath));
@@ -80,5 +83,24 @@ final class CacheConfigManager implements ConfigManagerInterface
     public function isActionEnabled(string $entityName, string $view, string $action): bool
     {
         return $this->configManager->isActionEnabled($entityName, $view, $action);
+    }
+
+    public function isOptional(): bool
+    {
+        return false;
+    }
+
+    public function warmUp($cacheDir): void
+    {
+        $this->cache->deleteItem(self::CACHE_KEY);
+
+        try {
+            // this forces the full processing of the backend configuration
+            $this->getBackendConfig();
+        } catch (\PDOException $e) {
+            // this occurs for example when the database doesn't exist yet and the
+            // project is being installed ('composer install' clears the cache at the end)
+            // ignore this error at this point and display an error message later
+        }
     }
 }

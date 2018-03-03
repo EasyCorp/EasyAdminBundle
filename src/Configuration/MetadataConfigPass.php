30b56e63a9030da32f65745ff607437cfbf9ddc7
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-03-03 19:57:44 +0100

    Reverted a wrong change

--- src/Configuration/MetadataConfigPass.php
+++ src/Configuration/MetadataConfigPass.php
@@ -25,8 +25,14 @@ class MetadataConfigPass implements ConfigPassInterface
     public function process(array $backendConfig)
     {
         foreach ($backendConfig['entities'] as $entityName => $entityConfig) {
-            if (null === $em = $this->doctrine->getManagerForClass($entityConfig['class'])) {
-                throw new InvalidTypeException(sprintf('The configured class "%s" for the path "easy_admin.entities.%s" is no mapped entity. Did you forget to create the entity class or to define its namespace?', $entityConfig['class'], $entityName));
+            try {
+                $em = $this->doctrine->getManagerForClass($entityConfig['class']);
+            } catch (\ReflectionException $e) {
+                throw new InvalidTypeException(sprintf('The configured class "%s" for the path "easy_admin.entities.%s" does not exist. Did you forget to create the entity class or to define its namespace?', $entityConfig['class'], $entityName));
+            }
+
+            if (null === $em) {
+                throw new InvalidTypeException(sprintf('The configured class "%s" for the path "easy_admin.entities.%s" is no mapped entity.', $entityConfig['class'], $entityName));
             }
 
             $entityMetadata = $em->getMetadataFactory()->getMetadataFor($entityConfig['class']);

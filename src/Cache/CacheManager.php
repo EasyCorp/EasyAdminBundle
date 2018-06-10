1d7df1c7338855288d4625980b97908a23f36c5a
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-06-10 10:42:43 +0200

    Fixed code syntax

--- src/Cache/CacheManager.php
+++ src/Cache/CacheManager.php
@@ -22,12 +22,12 @@ class CacheManager extends FilesystemCache
 {
     public function __construct($cacheDir)
     {
-      if (isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'dev') {
-        parent::__construct($cacheDir, self::EXTENSION, 0000);
-      }
-      else {
-        parent::__construct($cacheDir);
-      }
+        if (isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'dev') {
+            parent::__construct($cacheDir, self::EXTENSION, 0000);
+        }
+        else {
+            parent::__construct($cacheDir);
+        }
     }
 
     /**

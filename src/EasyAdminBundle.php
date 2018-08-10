f0ba63304b07d72443aab9973e00ca6faee43adf
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-08-10 15:50:37 +0200

    Prepare the 1.17.14 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -22,7 +22,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    const VERSION = '1.17.14-DEV';
+    const VERSION = '1.17.14';
 
     public function build(ContainerBuilder $container)
     {

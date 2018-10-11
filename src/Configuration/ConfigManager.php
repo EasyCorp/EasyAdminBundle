84a5f0e613eae1bd4970b9d8b2c183618013f8ea
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-10-11 22:05:05 +0200

    Simplified the configuration management and processing

--- src/Configuration/ConfigManager.php
+++ src/Configuration/ConfigManager.php
@@ -3,23 +3,29 @@
 namespace EasyCorp\Bundle\EasyAdminBundle\Configuration;
 
 use EasyCorp\Bundle\EasyAdminBundle\Exception\UndefinedEntityException;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
-final class ConfigManager implements ConfigManagerInterface
+final class ConfigManager
 {
+    private const CACHE_KEY = 'easyadmin.processed_config';
+
     /** @var array */
     private $backendConfig;
-    /** @var PropertyAccessorInterface */
+    private $debug;
     private $propertyAccessor;
+    private $cache;
     /** @var array */
     private $originalBackendConfig;
     /** @var ConfigPassInterface[] */
     private $configPasses;
 
-    public function __construct(PropertyAccessorInterface $propertyAccessor, array $originalBackendConfig)
+    public function __construct(array $originalBackendConfig, bool $debug, PropertyAccessorInterface $propertyAccessor,  CacheItemPoolInterface $cache)
     {
-        $this->propertyAccessor = $propertyAccessor;
         $this->originalBackendConfig = $originalBackendConfig;
+        $this->debug = $debug;
+        $this->propertyAccessor = $propertyAccessor;
+        $this->cache = $cache;
     }
 
     /**
@@ -35,9 +41,7 @@ final class ConfigManager implements ConfigManagerInterface
      */
     public function getBackendConfig(string $propertyPath = null)
     {
-        if (null === $this->backendConfig) {
-            $this->backendConfig = $this->doProcessConfig($this->originalBackendConfig);
-        }
+        $this->backendConfig = $this->loadBackendConfig();
 
         if (empty($propertyPath)) {
             return $this->backendConfig;
@@ -114,6 +118,25 @@ final class ConfigManager implements ConfigManagerInterface
         foreach ($this->configPasses as $configPass) {
             $backendConfig = $configPass->process($backendConfig);
         }
+
+        return $backendConfig;
+    }
+
+    private function loadBackendConfig(): array
+    {
+        if (true === $this->debug) {
+            return $this->doProcessConfig($this->originalBackendConfig);
+        }
+
+        $cachedBackendConfig = $this->cache->getItem(self::CACHE_KEY);
+
+        if ($cachedBackendConfig->isHit()) {
+            return $cachedBackendConfig->get();
+        }
+
+        $backendConfig = $this->doProcessConfig($this->originalBackendConfig);
+        $cachedBackendConfig->set($backendConfig);
+        $this->cache->save($cachedBackendConfig);
 
         return $backendConfig;
     }

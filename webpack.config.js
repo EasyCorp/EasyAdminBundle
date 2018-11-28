ecd4db9bd4b4f60a2e73598bf4ed7902019868ca
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-11-28 20:28:04 +0100

    Include the select2 i18n files

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,15 @@ Encore
     .configureCssLoader(options => { options.minimize = false; })
     .enablePostCssLoader()
 
+    // copy select2 i18n files
+    .copyFiles({
+        from: './node_modules/select2/dist/js/i18n/',
+        // relative to the output dir
+        to: 'select2/i18n/[name].[ext]',
+        // only copy files matching this pattern
+        pattern: /\.js$/
+    })
+
     .addEntry('app', './assets/js/app.js')
     .addEntry('app-rtl', './assets/js/app-rtl.js')
     .addEntry('bootstrap-all', './assets/js/bootstrap-all.js')

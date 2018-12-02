70b9dca12b221fe0a226cebca8721e8f127d66ec
Javier Eguiluz <javier.eguiluz@gmail.com>
2018-12-02 18:49:35 +0100

    Removed the reference to the inexistent cache warmer service

--- src/DependencyInjection/EasyAdminExtension.php
+++ src/DependencyInjection/EasyAdminExtension.php
@@ -34,8 +34,6 @@ class EasyAdminExtension extends Extension
         if ($container->getParameter('kernel.debug')) {
             // in 'dev', use the built-in Symfony exception listener
             $container->removeDefinition('easyadmin.listener.exception');
-            // avoid parsing the entire config in 'dev' (even for requests unrelated to the backend)
-            $container->removeDefinition('easyadmin.cache.config_warmer');
         }
 
         if ($container->hasParameter('locale')) {

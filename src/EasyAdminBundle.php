49a7f6390964442c210d1246a8d595bb245561ea
Javier Eguiluz <javier.eguiluz@gmail.com>
2019-01-23 20:50:56 +0100

    Bumped development version

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '2.0.3';
+    public const VERSION = '2.0.4-DEV';
 
     public function build(ContainerBuilder $container)
     {

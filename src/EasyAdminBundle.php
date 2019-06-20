24acc6ab32a7904e3da7f4fc47f4b378c66215ee
Javier Eguiluz <javier.eguiluz@gmail.com>
2019-06-20 19:54:30 +0200

    Prepared the 2.2.0 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '2.1.5-DEV';
+    public const VERSION = '2.2.0';
 
     public function build(ContainerBuilder $container)
     {

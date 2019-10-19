a00ccccb408ab2019759b140c758f8ab5e38f7e2
Javier Eguiluz <javier.eguiluz@gmail.com>
2019-10-19 20:08:10 +0200

    Added a default action in the abstract dashboard controller

--- src/Dashboard/AbstractDashboard.php
+++ src/Dashboard/AbstractDashboard.php
@@ -4,6 +4,8 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Dashboard;
 
 use EasyCorp\Bundle\EasyAdminBundle\Menu\MenuItem;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * This class is useful to extend your dashboard from it instead of implementing
@@ -19,5 +21,13 @@ abstract class AbstractDashboard extends AbstractController implements Dashboard
     public function getMenuItems(): iterable
     {
         yield MenuItem::new('Dashboard', 'fa-home')->homepage();
+    }
+
+    /**
+     * @Route("/admin", name="dashboard")
+     */
+    public function index(): Response
+    {
+        return $this->render('@EasyAdmin/default/layout.html.twig');
     }
 }

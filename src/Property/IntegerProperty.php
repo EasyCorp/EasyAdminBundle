bd3e556cfe91863278f056027343c9fdc25c4244
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-01-28 19:57:41 +0100

    Added the IntegerProperty

---
+++ src/Property/IntegerProperty.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EasyCorp\Bundle\EasyAdminBundle\Property;
+
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Property\PropertyConfigInterface;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class IntegerProperty implements PropertyConfigInterface
+{
+    use PropertyConfigTrait;
+
+    public function __construct()
+    {
+        $this
+            ->setType('integer')
+            ->setFormType(IntegerType::class)
+            ->setTemplateName('property/string');
+    }
+}

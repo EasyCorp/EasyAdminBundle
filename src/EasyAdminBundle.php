60d134e915581b61ad5b810377e0ffac28134ed1
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-04-19 17:26:19 +0200

    Prepare 3.0.0-alpha1 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -9,14 +9,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '3.0.0-DEV';
+    public const VERSION = '3.0.0-alpha1';
     public const CONTEXT_ATTRIBUTE_NAME = 'easyadmin_context';
-
-    // TODO: remove this method when EasyAdmin 3 is released.
-    public function boot()
-    {
-        if ('cli' !== \PHP_SAPI) {
-            throw new \RuntimeException('You are trying to use EasyAdmin 3 in your project. However, that version is not ready yet to test it in real projects. Instead, use EasyAdmin 2.x, which is the latest stable version.');
-        }
-    }
 }

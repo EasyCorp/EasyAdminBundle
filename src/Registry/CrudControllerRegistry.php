db378b207f1cfd60f33ba56c547e81dd0827e107
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-04-25 16:11:22 +0200

    Removed an unused variable

--- src/Registry/CrudControllerRegistry.php
+++ src/Registry/CrudControllerRegistry.php
@@ -9,40 +9,40 @@ use EasyCorp\Bundle\EasyAdminBundle\Contracts\Controller\CrudControllerInterface
  */
 final class CrudControllerRegistry
 {
-    /** @var CrudControllerInterface[] */
-    private $crudControllers;
-    private $controllerToEntityMap;
-    private $entityToControllerMap;
+    private $controllerFqcnToEntityFqcnMap;
+    private $entityFqcnToControllerFqcnMap;
 
     private function __construct()
     {
     }
 
+    /**
+     * @param CrudControllerInterface[] $crudControllers
+     */
     public static function new(iterable $crudControllers): self
     {
         $registry = new self();
-        $registry->crudControllers = iterator_to_array($crudControllers);
 
-        foreach ($registry->crudControllers as $controller) {
+        foreach (iterator_to_array($crudControllers) as $controller) {
             $controllerFqcn = \get_class($controller);
-            $registry->controllerToEntityMap[$controllerFqcn] = $controller::getEntityFqcn();
+            $registry->controllerFqcnToEntityFqcnMap[$controllerFqcn] = $controller::getEntityFqcn();
         }
 
         // more than one controller can manage the same entity, so this map will
-        // only contain the last controller mapped to that repeated entity. That's why
+        // only contain the last controller associated to that repeated entity. That's why
         // several methods in other classes allow to define the CRUD controller explicitly
-        $registry->entityToControllerMap = array_flip($registry->controllerToEntityMap);
+        $registry->entityFqcnToControllerFqcnMap = array_flip($registry->controllerFqcnToEntityFqcnMap);
 
         return $registry;
     }
 
     public function getControllerFqcnByEntityFqcn(string $entityFqcn): ?string
     {
-        return $this->entityToControllerMap[$entityFqcn] ?? null;
+        return $this->entityFqcnToControllerFqcnMap[$entityFqcn] ?? null;
     }
 
     public function getEntityFqcnByControllerFqcn(string $controllerFqcn): ?string
     {
-        return $this->controllerToEntityMap[$controllerFqcn] ?? null;
+        return $this->controllerFqcnToEntityFqcnMap[$controllerFqcn] ?? null;
     }
 }

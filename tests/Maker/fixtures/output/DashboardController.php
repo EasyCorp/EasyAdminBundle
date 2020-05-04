3f8998aa5372fcf25686ec83a7925a5cb103bf2e
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-05-04 20:28:48 +0200

    Improved the migrator command to update the FontAwesome icon names

--- tests/Maker/fixtures/output/DashboardController.php
+++ tests/Maker/fixtures/output/DashboardController.php
@@ -27,18 +27,18 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         $submenu1 = [
-            MenuItem::linkToCrud('menu.product.list', 'fa fa-th-list', Product::class)->setDefaultSort(['createdAt' => 'DESC']),
-            MenuItem::linkToCrud('menu.category', 'fa fa-tags', Category::class),
-            MenuItem::linkToCrud('menu.product.add', 'fa fa-plus-circle', Product::class),
+            MenuItem::linkToCrud('menu.product.list', 'fas fa-th-list', Product::class)->setDefaultSort(['createdAt' => 'DESC']),
+            MenuItem::linkToCrud('menu.category', 'fas fa-tags', Category::class),
+            MenuItem::linkToCrud('menu.product.add', 'fas fa-plus-circle', Product::class),
         ];
 
-        yield MenuItem::subMenu('menu.product', 'fa fa-shopping-basket')->setSubItems($submenu1);
-        yield MenuItem::linkToCrud('menu.customer', 'fa fa-users', User::class);
-        yield MenuItem::linkToCrud('menu.purchase', 'fa fa-credit-card', Purchase::class)->setDefaultSort(['deliveryDate' => 'DESC']);
+        yield MenuItem::subMenu('menu.product', 'fas fa-shopping-basket')->setSubItems($submenu1);
+        yield MenuItem::linkToCrud('menu.customer', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('menu.purchase', 'far fa-credit-card', Purchase::class)->setDefaultSort(['deliveryDate' => 'DESC']);
 
-        yield MenuItem::section('menu.about', 'fa fa-folder-open');
-        yield MenuItem::linkToUrl('menu.about.home', 'fa fa-home', 'https://github.com/EasyCorp/EasyAdminBundle')->setLinkTarget('_blank')->setLinkRel('noreferrer');
-        yield MenuItem::linkToUrl('menu.about.docs', 'fa fa-book', 'https://symfony.com/doc/current/bundles/EasyAdminBundle')->setLinkTarget('_blank')->setLinkRel('noreferrer');
-        yield MenuItem::linkToUrl('menu.about.issues', 'fa fa-github', 'https://github.com/EasyCorp/EasyAdminBundle/issues')->setLinkTarget('_blank')->setLinkRel('noreferrer');
+        yield MenuItem::section('menu.about', 'fas fa-folder-open');
+        yield MenuItem::linkToUrl('menu.about.home', 'fas fa-home', 'https://github.com/EasyCorp/EasyAdminBundle')->setLinkTarget('_blank')->setLinkRel('noreferrer');
+        yield MenuItem::linkToUrl('menu.about.docs', 'fas fa-book', 'https://symfony.com/doc/current/bundles/EasyAdminBundle')->setLinkTarget('_blank')->setLinkRel('noreferrer');
+        yield MenuItem::linkToUrl('menu.about.issues', 'fab fa-github', 'https://github.com/EasyCorp/EasyAdminBundle/issues')->setLinkTarget('_blank')->setLinkRel('noreferrer');
     }
 }

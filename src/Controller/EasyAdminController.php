f0e61484e1f96962c5944c1ef442d72e863dd718
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-05-30 15:48:13 +0200

    Improved the error message about upgrading EA2 to EA3

--- src/Controller/EasyAdminController.php
+++ src/Controller/EasyAdminController.php
@@ -20,8 +20,14 @@ One of those removed classes is "EasyAdminController", which was used in EasyAdm
 as the base controller of the entire backend. In EasyAdmin 3 each Doctrine entity has
 its own controller, so you can no longer use that base controller.
 
-This file is usually referenced from the "config/routes/easy_admin.yaml" file. Remove
-or update that configuration file to stop using the removed controller class.
+If your application config or code references to this removed controller, you'll see
+this error message. Check out the following files:
+
+1) Remove the "config/routes/easy_admin.yaml" file. This is no longer needed in EasyAdmin 3.
+
+2) Check the contents of "config/routes.yaml" and remove any route related to that controller.
+
+3) Check that your own admin controllers don't extend from the "EasyAdminController" class.
 
 If you need more help, read the EasyAdmin 3 documentation at:
 https://symfony.com/doc/master/bundles/EasyAdminBundle/index.html

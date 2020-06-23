97ee1140ef9a92a129022d7b6bfe1171db5bc1a3
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-06-23 20:56:32 +0200

    Prepare 3.0.1 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -9,6 +9,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '3.0.1-DEV';
+    public const VERSION = '3.0.1';
     public const CONTEXT_ATTRIBUTE_NAME = 'easyadmin_context';
 }

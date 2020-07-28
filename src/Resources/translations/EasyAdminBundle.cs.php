3ea4eec62eadf0241b83dba20035df03616516c3
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-07-28 20:09:07 +0200

    Fixed a Czech translation

--- src/Resources/translations/EasyAdminBundle.cs.php
+++ src/Resources/translations/EasyAdminBundle.cs.php
@@ -100,7 +100,7 @@ return [
 
     'form' => [
         'are_you_sure' => 'Neuložili jste změny provedené v tomto formuláři.',
-        'tab.error_badge_title' => '{1} Jeden neplatný vstup|{2,3,4} %count% neplatné vstupy|]5,Inf] %count% neplatných vstupů',
+        'tab.error_badge_title' => '{1} Jeden neplatný vstup|{2,3,4} %count% neplatné vstupy|[5,Inf] %count% neplatných vstupů',
     ],
 
     'user' => [

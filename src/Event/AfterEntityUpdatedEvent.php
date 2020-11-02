2ec78055f1d05268bda2421218a1632ce35183d6
Tarjei Huse <tarjei.huse@gmail.com>
2020-11-02 20:51:51 +0100

    Documet when the event is triggered.

--- src/Event/AfterEntityUpdatedEvent.php
+++ src/Event/AfterEntityUpdatedEvent.php
@@ -3,6 +3,12 @@
 namespace EasyCorp\Bundle\EasyAdminBundle\Event;
 
 /**
+ * This event is triggered after the updateEntity() call in the 
+ * new() or edit() flows.
+ *
+ * @see AbstractCrudController::edit
+ * @see AbstractCrudController::new
+ *
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
 final class AfterEntityUpdatedEvent

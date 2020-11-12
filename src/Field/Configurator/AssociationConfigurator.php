3cc9c8a6c1cf6d51581697a97ea10690a40462a0
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-11-12 17:50:44 +0100

    Don't override the query_builder form option in associations

--- src/Field/Configurator/AssociationConfigurator.php
+++ src/Field/Configurator/AssociationConfigurator.php
@@ -81,7 +81,7 @@ final class AssociationConfigurator implements FieldConfiguratorInterface
 
             $field->setFormTypeOption('attr.data-ea-autocomplete-endpoint-url', $autocompleteEndpointUrl);
         } else {
-            $field->setFormTypeOption('query_builder', static function (EntityRepository $repository) use ($field) {
+            $field->setFormTypeOptionIfNotSet('query_builder', static function (EntityRepository $repository) use ($field) {
                 // TODO: should this use `createIndexQueryBuilder` instead, so we get the default ordering etc.?
                 // it would then be identical to the one used in autocomplete action, but it is a bit complex getting it in here
                 $queryBuilder = $repository->createQueryBuilder('entity');

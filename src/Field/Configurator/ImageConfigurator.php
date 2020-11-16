60bd1978c0192191c9ef16bfd556a6fa80c4d566
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-11-16 17:14:31 +0100

    Allow to upload/remove images in Image fields

--- src/Field/Configurator/ImageConfigurator.php
+++ src/Field/Configurator/ImageConfigurator.php
@@ -7,12 +7,20 @@ use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldConfiguratorInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use function Symfony\Component\String\u;
 
 /**
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
 final class ImageConfigurator implements FieldConfiguratorInterface
 {
+    private $projectDir;
+
+    public function __construct(string $projectDir)
+    {
+        $this->projectDir = $projectDir;
+    }
+
     public function supports(FieldDto $field, EntityDto $entityDto): bool
     {
         return ImageField::class === $field->getFieldFqcn();
@@ -20,10 +28,16 @@ final class ImageConfigurator implements FieldConfiguratorInterface
 
     public function configure(FieldDto $field, EntityDto $entityDto, AdminContext $context): void
     {
+        $relativeUploadDir = $field->getCustomOption(ImageField::OPTION_UPLOAD_DIR) ?? 'public/uploads/images/';
+        $relativeUploadDir = u($relativeUploadDir)->trimStart(\DIRECTORY_SEPARATOR)->ensureEnd(\DIRECTORY_SEPARATOR)->toString();
+        $absoluteUploadDir = u($relativeUploadDir)->ensureStart($this->projectDir.\DIRECTORY_SEPARATOR)->toString();
+        $field->setFormTypeOption('upload_dir', $absoluteUploadDir);
+
         $configuredBasePath = $field->getCustomOption(ImageField::OPTION_BASE_PATH);
         $formattedValue = $this->getImagePath($field->getValue(), $configuredBasePath);
-
         $field->setFormattedValue($formattedValue);
+
+        $field->setFormTypeOption('upload_filename', $field->getCustomOption(ImageField::OPTION_UPLOADED_FILE_NAME_PATTERN));
 
         // this check is needed to avoid displaying broken images when image properties are optional
         if (empty($formattedValue) || $formattedValue === rtrim($configuredBasePath ?? '', '/')) {
@@ -37,6 +51,9 @@ final class ImageConfigurator implements FieldConfiguratorInterface
         if (null === $imagePath || 0 !== preg_match('/^(http[s]?|\/\/)/i', $imagePath)) {
             return $imagePath;
         }
+
+        // remove project path from filepath
+        $imagePath = str_replace($this->projectDir.\DIRECTORY_SEPARATOR.'public'.\DIRECTORY_SEPARATOR, '', $imagePath);
 
         return isset($basePath)
             ? rtrim($basePath, '/').'/'.ltrim($imagePath, '/')

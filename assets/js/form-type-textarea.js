f3c0793e4c94f60d4b798abde0a122a06558de9e
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-11-17 20:03:33 +0100

    Added a comment explaining why we need the condition

--- assets/js/form-type-textarea.js
+++ assets/js/form-type-textarea.js
@@ -10,6 +10,9 @@ class Autogrow {
         this.field.style.resize = 'none';
         this.field.style.boxSizing = 'border-box';
         this.field.style.height = 'auto';
+
+        // this check is needed because the <textarea> element can be inside a
+        // minimizable panel, causing its scrollHeight value to be 0
         if (this.field.scrollHeight > 0) {
             this.field.style.height = this.field.scrollHeight + 'px';
         }

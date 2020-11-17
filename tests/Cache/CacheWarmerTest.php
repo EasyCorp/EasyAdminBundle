827e267d6185580335bf01cafdd3279690502e74
Javier Eguiluz <javier.eguiluz@gmail.com>
2020-11-17 20:46:13 +0100

    Added some tests for the cache warmer

---
+++ tests/Cache/CacheWarmerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace EasyCorp\Bundle\EasyAdminBundle\Tests\Cache;
+
+use EasyCorp\Bundle\EasyAdminBundle\Cache\CacheWarmer;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+class CacheWarmerTest extends TestCase
+{
+    private $cacheDirectory;
+    private $dashboardRoutesCacheFile;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->cacheDirectory = sprintf('%s/cache_dir_%d/', sys_get_temp_dir(), random_int(1, 999999));
+        $this->dashboardRoutesCacheFile = $this->cacheDirectory.CacheWarmer::DASHBOARD_ROUTES_CACHE;
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        (new Filesystem())->remove($this->cacheDirectory);
+    }
+
+    public function testWarmUpWithNoRoutes()
+    {
+        $router = $this->getMockBuilder(RouterInterface::class)->getMock();
+        $router->method('getRouteCollection')->willReturn([]);
+
+        $cacheWarmer = new CacheWarmer($router);
+        $cacheWarmer->warmUp($this->cacheDirectory);
+
+        $this->assertCachedRoutesEqual([]);
+    }
+
+    public function testWarmUp()
+    {
+        $router = $this->getMockBuilder(RouterInterface::class)->getMock();
+        $router->method('getRouteCollection')
+            ->willReturn([
+                'admin1' => new Route('/admin1', ['_controller' => TestingDashboardController::class.'::index']),
+                'admin2' => new Route('/admin2', ['_controller' => TestingDashboardController::class]),
+                'admin3' => new Route('/admin3', ['_controller' => [TestingDashboardController::class, 'index']]),
+                'admin4' => new Route('/admin4', ['_controller' => [TestingDashboardController::class]]),
+                'admin5' => new Route('/admin5', ['_controller' => TestingDashboardController::class.'::someMethod']),
+                'admin6' => new Route('/admin6', ['_controller' => [TestingDashboardController::class, 'someMethod']]),
+            ]);
+
+        $cacheWarmer = new CacheWarmer($router);
+        $cacheWarmer->warmUp($this->cacheDirectory);
+
+        $this->assertCachedRoutesEqual([
+            'admin1' => TestingDashboardController::class.'::index',
+            'admin2' => TestingDashboardController::class.'::__invoke',
+            'admin3' => TestingDashboardController::class.'::index',
+            'admin4' => TestingDashboardController::class.'::__invoke',
+        ]);
+    }
+
+    private function assertCachedRoutesEqual(array $expectedCachedRoutes)
+    {
+        $this->assertStringEqualsFile($this->dashboardRoutesCacheFile, '<?php return '.var_export($expectedCachedRoutes, true).';');
+    }
+}
+
+final class TestingDashboardController extends AbstractDashboardController
+{
+}

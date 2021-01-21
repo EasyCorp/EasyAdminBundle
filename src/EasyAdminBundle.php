a51b3e5eaa5bfafda9ed2791d2a2e0735c845893
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-01-21 19:39:17 +0100

    Prepare 2.3.12 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '2.3.12-DEV';
+    public const VERSION = '2.3.12';
 
     public function build(ContainerBuilder $container)
     {

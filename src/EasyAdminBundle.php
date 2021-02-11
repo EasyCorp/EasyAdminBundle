e0daee67b818e747af2e7d522bf363f5d81225a9
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-02-11 19:33:17 +0100

    Backported a 2.x fix for modern PHP versions

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -22,7 +22,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    const VERSION = '1.17.24';
+    const VERSION = '1.17.25';
 
     public function build(ContainerBuilder $container)
     {

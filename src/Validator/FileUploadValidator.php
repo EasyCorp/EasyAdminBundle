f7d729cd980ffc2cc8c812380ab7a4c7a01f8619
GaëlB <g.bordas@boginfo.fr>
2021-04-01 08:37:15 +0200

    Update FileUploadValidator.php
    Clean class (useless use and annotation bloc)

--- src/Validator/FileUploadValidator.php
+++ src/Validator/FileUploadValidator.php
@@ -3,13 +3,9 @@
 namespace EasyCorp\Bundle\EasyAdminBundle\Validator;
 
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\FileValidator;
 
-/**
- * @Annotation
- */
 class FileUploadValidator extends FileValidator
 {
     public function validate($value, Constraint $constraint)

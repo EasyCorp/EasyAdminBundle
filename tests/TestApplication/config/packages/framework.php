908c263922b051860b9ab97516922f34f4c1c7b4
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-09-18 11:00:22 +0200

    -

--- tests/TestApplication/config/packages/framework.php
+++ tests/TestApplication/config/packages/framework.php
@@ -7,7 +7,6 @@ $configuration = [
     'csrf_protection' => true,
     'session' => [
         'handler_id' => null,
-        'storage_factory_id' => 'session.storage.factory.mock_file',
     ],
     'test' => true,
 ];

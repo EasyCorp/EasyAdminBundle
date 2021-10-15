9fa093a1c0689861ae7182bb68aa475c8ebb88aa
Benjamin Leibinger <benjamin.leibinger@burda.com>
2021-10-15 20:51:39 +0200

    Add EntityLifecycleEventInterface to the Before/AfterEntity Events

--- src/Event/BeforeEntityDeletedEvent.php
+++ src/Event/BeforeEntityDeletedEvent.php
@@ -5,19 +5,7 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Event;
 /**
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
-final class BeforeEntityDeletedEvent
+final class BeforeEntityDeletedEvent extends AbstractLifecycleEvent
 {
     use StoppableEventTrait;
-
-    private $entityInstance;
-
-    public function __construct($entityInstance)
-    {
-        $this->entityInstance = $entityInstance;
-    }
-
-    public function getEntityInstance()
-    {
-        return $this->entityInstance;
-    }
 }

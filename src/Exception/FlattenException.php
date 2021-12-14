9205a18163331f8a243305a416c3568647f2cc55
Javier Eguiluz <javier.eguiluz@gmail.com>
2021-12-14 20:28:20 +0100

    Fix some types in FlattenException class methods

--- src/Exception/FlattenException.php
+++ src/Exception/FlattenException.php
@@ -13,14 +13,7 @@ final class FlattenException extends BaseFlattenException
     /** @var ExceptionContext */
     private $context;
 
-    /**
-     * @param int $statusCode
-     *
-     * @return FlattenException
-     *
-     * @throws \RuntimeException
-     */
-    public static function create(\Exception $exception, $statusCode = null, array $headers = []): BaseFlattenException
+    public static function create(\Exception $exception, ?int $statusCode = null, array $headers = []): static
     {
         if (!$exception instanceof BaseException) {
             throw new \RuntimeException(sprintf('You should only try to create an instance of "%s" with a "EasyCorp\Bundle\EasyAdminBundle\Exception\BaseException" instance, or subclass. "%s" given.', __CLASS__, \get_class($exception)));

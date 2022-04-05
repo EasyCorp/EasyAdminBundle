f4c9b3d1d510957cb6548aec4f48371a0a64416d
yacine <yacine.rerbal@hotmail.fr>
2022-04-05 10:52:45 +0200

    remove description in the constructor

--- src/Command/MakeAdminDashboardCommand.php
+++ src/Command/MakeAdminDashboardCommand.php
@@ -4,6 +4,7 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Command;
 
 use EasyCorp\Bundle\EasyAdminBundle\Maker\ClassMaker;
 use Symfony\Component\Console\Command\Command;
+
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -35,7 +36,6 @@ class MakeAdminDashboardCommand extends Command
         parent::__construct($name);
         $this->classMaker = $classMaker;
         $this->projectDir = $projectDir;
-        $this->description = Command::getDefaultDescription;
     }
 
     protected function configure()

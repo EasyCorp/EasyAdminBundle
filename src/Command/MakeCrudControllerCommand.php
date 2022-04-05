43af3c75bc73ebc2f1fd05e298431c517fa0c71c
yacine <yacine.rerbal@hotmail.fr>
2022-04-05 14:06:07 +0200

    php syntax fix

--- src/Command/MakeCrudControllerCommand.php
+++ src/Command/MakeCrudControllerCommand.php
@@ -23,7 +23,6 @@ use function Symfony\Component\String\u;
 )]
 class MakeCrudControllerCommand extends Command
 {
-
     private string $projectDir;
     private ClassMaker $classMaker;
     private ManagerRegistry $doctrine;

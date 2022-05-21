320f63f636bdbccc93a6bdeec404bb1e418f25a4
Javier Eguiluz <javier.eguiluz@gmail.com>
2022-05-21 18:49:48 +0200

    Move theme selector JavaScript code out of the template

--- assets/js/page-color-scheme.js
+++ assets/js/page-color-scheme.js
@@ -15,20 +15,28 @@ class ColorSchemeHandler {
             return;
         }
 
+        // for·responsive design" reasons, pages contain two color scheme selectors instead
+        // of one and the selected scheme must be kept in sync in both
+        const colorSchemeSelectors = document.querySelectorAll('.dropdown-appearance');
         const currentScheme = localStorage.getItem(this.#colorSchemeLocalStorageKey) || 'auto';
-        const selectorOptions = document.querySelectorAll('.dropdown-appearance a[data-ea-color-scheme]');
-        const selectorActiveOption = document.querySelector(`.dropdown-appearance a[data-ea-color-scheme="${ currentScheme }"]`);
 
-        selectorOptions.forEach((selector) => { selector.classList.remove('active') });
-        selectorActiveOption.classList.add('active');
+        colorSchemeSelectors.forEach((colorSchemeSelector) => {
+            const selectorOptions = colorSchemeSelector.querySelectorAll('.dropdown-appearance a[data-ea-color-scheme]');
+            const selectorActiveOption = colorSchemeSelector.querySelector(`.dropdown-appearance a[data-ea-color-scheme="${ currentScheme }"]`);
 
-        selectorOptions.forEach((selector) => {
-            selector.addEventListener('click', () => {
-                const selectedColorScheme = selector.getAttribute('data-ea-color-scheme');
-                this.#setColorScheme(selectedColorScheme);
+            selectorOptions.forEach((selector) => { selector.classList.remove('active') });
+            selectorActiveOption.classList.add('active');
 
-                selectorOptions.forEach((otherSelector) => { otherSelector.classList.remove('active') });
-                selector.classList.add('active');
+            selectorOptions.forEach((selector) => {
+                selector.addEventListener('click', () => {
+                    const selectedColorScheme = selector.getAttribute('data-ea-color-scheme');
+                    this.#setColorScheme(selectedColorScheme);
+
+                    const allSelectorOptions = document.querySelectorAll('.dropdown-appearance a[data-ea-color-scheme]');
+                    const allSelectorActiveOptions = document.querySelectorAll(`.dropdown-appearance a[data-ea-color-scheme="${ selectedColorScheme }"]`);
+                    allSelectorOptions.forEach((selectorOption) => { selectorOption.classList.remove('active') });
+                    allSelectorActiveOptions.forEach((selectorOption) => { selectorOption.classList.add('active') });
+                });
             });
         });
     }

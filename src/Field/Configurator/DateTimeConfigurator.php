8f4853818fdb8c2aeba917609c0c211b2da0fd57
andyexeter <palmer.andy@gmail.com>
2022-06-26 21:35:27 +0100

    Check for 'none' time format as well as 'short' when deciding whether to add step attribute

--- src/Field/Configurator/DateTimeConfigurator.php
+++ src/Field/Configurator/DateTimeConfigurator.php
@@ -84,7 +84,7 @@ final class DateTimeConfigurator implements FieldConfiguratorInterface
             $field->setFormTypeOption('widget', 'single_text');
             $field->setFormTypeOption('html5', true);
 
-            if (('' !== trim($icuDateTimePattern) && str_contains($icuDateTimePattern, 's')) || DateTimeField::FORMAT_SHORT !== $timeFormat) {
+            if (('' !== trim($icuDateTimePattern) && str_contains($icuDateTimePattern, 's')) || (DateTimeField::FORMAT_SHORT !== $timeFormat && DateTimeField::FORMAT_NONE !== $timeFormat)) {
                 $attr = $field->getFormTypeOption('attr') ?? [];
                 $attr['step'] = '1';
                 $field->setFormTypeOption('attr', $attr);

1c4c58cb172f7af525968486b5a1bd164bdb71cd
Javier Eguiluz <javier.eguiluz@gmail.com>
2022-09-02 20:03:46 +0200

    Bump development version

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '3.5.23';
+    public const VERSION = '3.5.24-DEV';
     /** @deprecated use EA::CONTEXT_REQUEST_ATTRIBUTE */
     public const CONTEXT_ATTRIBUTE_NAME = EA::CONTEXT_REQUEST_ATTRIBUTE;
 

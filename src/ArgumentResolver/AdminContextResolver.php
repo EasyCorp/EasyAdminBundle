734394a3c177258f735533c9c1173e675a1bd393
Javier Eguiluz <javier.eguiluz@gmail.com>
2022-10-28 20:28:32 +0200

    Update GitHub CI config for Symfony 6.2

--- src/ArgumentResolver/AdminContextResolver.php
+++ src/ArgumentResolver/AdminContextResolver.php
@@ -5,31 +5,50 @@ namespace EasyCorp\Bundle\EasyAdminBundle\ArgumentResolver;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
 /*
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
-final class AdminContextResolver implements CompatValueResolverInterface
-{
-    private AdminContextProvider $adminContextProvider;
-
-    public function __construct(AdminContextProvider $adminContextProvider)
+if (class_exists(ValueResolverInterface::class)) {
+    final class AdminContextResolver implements ValueResolverInterface
     {
-        $this->adminContextProvider = $adminContextProvider;
-    }
+        private AdminContextProvider $adminContextProvider;
 
-    public function supports(Request $request, ArgumentMetadata $argument): bool
-    {
-        return AdminContext::class === $argument->getType();
-    }
-
-    public function resolve(Request $request, ArgumentMetadata $argument): iterable
-    {
-        if (AdminContext::class !== $argument->getType()) {
-            return [];
+        public function __construct(AdminContextProvider $adminContextProvider)
+        {
+            $this->adminContextProvider = $adminContextProvider;
         }
 
-        yield $this->adminContextProvider->getContext();
+        public function resolve(Request $request, ArgumentMetadata $argument): iterable
+        {
+            if (AdminContext::class !== $argument->getType()) {
+                return [];
+            }
+
+            yield $this->adminContextProvider->getContext();
+        }
+    }
+} else {
+    final class AdminContextResolver implements ArgumentValueResolverInterface
+    {
+        private AdminContextProvider $adminContextProvider;
+
+        public function __construct(AdminContextProvider $adminContextProvider)
+        {
+            $this->adminContextProvider = $adminContextProvider;
+        }
+
+        public function supports(Request $request, ArgumentMetadata $argument): bool
+        {
+            return AdminContext::class === $argument->getType();
+        }
+
+        public function resolve(Request $request, ArgumentMetadata $argument): iterable
+        {
+            yield $this->adminContextProvider->getContext();
+        }
     }
 }

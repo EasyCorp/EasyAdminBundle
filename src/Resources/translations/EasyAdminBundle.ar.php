89da1d5b2ea6f349b1c2daf4acead484800a40e9
Nadim AL ABDOU <46465503+nalabdou@users.noreply.github.com>
2022-11-28 04:23:22 +0100

    Add missing Arabic translations

--- src/Resources/translations/EasyAdminBundle.ar.php
+++ src/Resources/translations/EasyAdminBundle.ar.php
@@ -100,6 +100,7 @@ return [
     'form' => [
         'are_you_sure' => 'لم يتّم حفظ التغييرات.',
         'tab.error_badge_title' => 'حقل واحد غير صالح|%count% حقول غير صالحة',
+        'slug.confirm_text' => 'إذا قمت بتغيير الرابط الثابت ، يمكنك تعطيل الروابط الموجودة في الصفحات الأخرى.',
     ],
 
     'user' => [
@@ -137,7 +138,7 @@ return [
 
     'autocomplete' => [
         'no-results-found' => 'لم يتم العثور على أي نتائج',
-        // 'no-more-results' => 'No more results',
+        'no-more-results' => 'لا يوجد نتائج أٌخرى',
         'loading-more-results' => 'جاري تحميل نتائج إضافية…',
     ],
 ];

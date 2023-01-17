02ca5f3f61bfc31538dfc4c27e0f8674f8ae4409
ksn135 <serg@kalachev.ru>
2023-01-17 22:14:54 +0300

    Fix The "Symfony\Component\Security\Core\Security" service alias is deprecated

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -87,7 +87,7 @@ use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\SecurityBundle\Security;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services()

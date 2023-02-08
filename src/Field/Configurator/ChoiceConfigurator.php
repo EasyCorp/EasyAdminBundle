6a2689e0cea44cf5faea4b7face9ed88ce4da999
Javier Eguiluz <javier.eguiluz@gmail.com>
2023-02-08 20:18:52 +0100

    Fix some issues in Choice field with PHP Enums

--- src/Field/Configurator/ChoiceConfigurator.php
+++ src/Field/Configurator/ChoiceConfigurator.php
@@ -10,6 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Translation\TranslatableChoiceMessage;
 use EasyCorp\Bundle\EasyAdminBundle\Translation\TranslatableChoiceMessageCollection;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use function Symfony\Component\String\u;
 use function Symfony\Component\Translation\t;
 use Symfony\Contracts\Translation\TranslatableInterface;
@@ -55,6 +56,8 @@ final class ChoiceConfigurator implements FieldConfiguratorInterface
             $allChoicesAreEnums = false === \in_array(false, $elementIsEnum, true);
 
             if ($allChoicesAreEnums) {
+                $field->setFormType(EnumType::class);
+
                 $processedEnumChoices = [];
                 foreach ($choices as $choice) {
                     if ($choice instanceof \BackedEnum) {

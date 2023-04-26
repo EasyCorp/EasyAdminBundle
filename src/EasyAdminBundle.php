9fe02eba177e4f43551db8770197e289394e5806
Javier Eguiluz <javier.eguiluz@gmail.com>
2023-04-26 20:12:05 +0200

    Prepare 4.6.2 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '4.6.2-DEV';
+    public const VERSION = '4.6.2';
 
     public function build(ContainerBuilder $container): void
     {

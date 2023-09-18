1f491890705b6dc58acccb93f44fb2f27d404d4b
Mathieu Santostefano <msantostefano@protonmail.com>
2023-09-18 19:57:32 +0200

    fix(trix): fix trix editor config loading

--- assets/js/field-text-editor.js
+++ assets/js/field-text-editor.js
@@ -6,7 +6,9 @@ import Trix from 'trix/dist/trix';
 // Provide Trix variable globally to allow custom backend pages to use it
 window.Trix = Trix;
 
-document.addEventListener('DOMContentLoaded', () => {
+// Listening to the DOMLoadedContent event is too late because the Trix editor is already initialized.
+// To be sure to handle properly the custom configuration, we have to to listen to the trix-before-initialize event.
+document.addEventListener('trix-before-initialize', () => {
     new TextEditorField();
 });
 

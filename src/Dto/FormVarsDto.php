08346a21ec35f98441603389392062b6ba2e37e3
Javier Eguiluz <javier.eguiluz@gmail.com>
2023-10-16 20:26:33 +0200

    -

--- src/Dto/FormVarsDto.php
+++ src/Dto/FormVarsDto.php
@@ -3,6 +3,10 @@
 namespace EasyCorp\Bundle\EasyAdminBundle\Dto;
 
 /**
+ * It stores the variables related to EasyAdmin that are passed to all
+ * the form types templates via the `form.vars.ea_vars` variable. It's a similar
+ * concept to the variables passed by Symfony via the `form.vars` variable.
+ *
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
 final class FormVarsDto

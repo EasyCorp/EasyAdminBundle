0bfcda2cc2b27f49f6647128a99ca11355952b0d
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2024-01-04 15:05:39 +0100

    Removed phpdoc from interface

--- src/Provider/AdminContextProviderInterface.php
+++ src/Provider/AdminContextProviderInterface.php
@@ -6,9 +6,6 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Provider;
 
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 
-/**
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
- */
 interface AdminContextProviderInterface
 {
     public function getContext(): ?AdminContext;

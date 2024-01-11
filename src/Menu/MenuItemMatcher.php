56eb177883c5fdb96878ac550306bb61cfb74ac3
Javier Eguiluz <javier.eguiluz@gmail.com>
2024-01-11 19:30:14 +0100

    Minor tweaks

--- src/Menu/MenuItemMatcher.php
+++ src/Menu/MenuItemMatcher.php
@@ -25,12 +25,9 @@ class MenuItemMatcher implements MenuItemMatcherInterface
         }
 
         $currentPageQueryParameters = $adminContext->getRequest()->query->all();
-        $menuItemQueryString = null === $menuItemDto->getLinkUrl()
-            ? null
-            : parse_url($menuItemDto->getLinkUrl(), \PHP_URL_QUERY);
+        $menuItemQueryString = null === $menuItemDto->getLinkUrl() ? null : parse_url($menuItemDto->getLinkUrl(), \PHP_URL_QUERY);
 
         $menuItemQueryParameters = [];
-
         if (true === \is_string($menuItemQueryString)) {
             parse_str($menuItemQueryString, $menuItemQueryParameters);
         }

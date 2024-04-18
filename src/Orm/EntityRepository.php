4cd3b8f973a891f42a1c14a9420fba6f7efd02ed
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-04-18 08:50:43 +0200

    Revert "Fixed invalid DQL"
    This reverts commit 6511215ad4f3ce9c9dd15e6531c250b6cc87ccae.

--- src/Orm/EntityRepository.php
+++ src/Orm/EntityRepository.php
@@ -120,7 +120,7 @@ final class EntityRepository implements EntityRepositoryInterface
                     $parameterName = sprintf('query_for_ulids_%d', $queryTermIndex);
                     $queryTermConditions->add(sprintf('%s.%s = :%s', $entityName, $propertyConfig['property_name'], $parameterName));
                     $queryBuilder->setParameter($parameterName, $dqlParameters['uuid_query'], 'ulid');
-                } elseif ($propertyConfig['is_text'] || $propertyConfig['is_integer']) {
+                } elseif ($propertyConfig['is_text']) {
                     $parameterName = sprintf('query_for_text_%d', $queryTermIndex);
                     // concatenating an empty string is needed to avoid issues on PostgreSQL databases (https://github.com/EasyCorp/EasyAdminBundle/issues/6290)
                     $queryTermConditions->add(sprintf('LOWER(CONCAT(%s.%s, \'\')) LIKE :%s', $entityName, $propertyConfig['property_name'], $parameterName));

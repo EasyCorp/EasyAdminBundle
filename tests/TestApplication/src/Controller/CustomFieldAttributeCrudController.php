12c79d8591ca9e44247ed417a8cb99f98b8f4b93
Javier Eguiluz <javier.eguiluz@gmail.com>
2024-10-19 13:08:15 +0200

    Finished the feature

--- tests/TestApplication/src/Controller/CustomFieldAttributeCrudController.php
+++ tests/TestApplication/src/Controller/CustomFieldAttributeCrudController.php
@@ -2,47 +2,31 @@
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\Controller;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Tests\TestApplication\Entity\BlogPost;
 
-/**
- * Tests the configureActions() method and the generated actions.
- */
 class CustomFieldAttributeCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Category::class;
+        return BlogPost::class;
     }
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            TextField::new('name')
-                ->setHtmlAttribute('multi-test-one', 'test1')
-                ->setHtmlAttribute('multi-test-two', 'test2'),
-        ];
-    }
+        yield TextField::new('title')
+            ->setHtmlAttribute('multi-test-one', 'test1')
+            ->setHtmlAttribute('multi-test-two', 'test2');
 
-    public function configureActions(Actions $actions): Actions
-    {
-        $action1 = Action::new('action1')->linkToCrudAction('');
-        $action2 = Action::new('action2')->linkToCrudAction('')->setCssClass('foo');
-        $action3 = Action::new('action3')->linkToCrudAction('')->addCssClass('bar');
-        $action4 = Action::new('action4')->linkToCrudAction('')->setCssClass('foo')->addCssClass('bar');
-
-        return $actions
-            ->add(Crud::PAGE_INDEX, $action1)
-            ->add(Crud::PAGE_INDEX, $action2)
-            ->add(Crud::PAGE_INDEX, $action3)
-            ->add(Crud::PAGE_INDEX, $action4)
-            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
-                return $action->setIcon('fa fa-fw fa-plus')->setLabel(false);
-            })
-        ;
+        yield DateTimeField::new('publishedAt')
+            ->setHtmlAttributes([
+                'data-some-attribute' => 'some-value',
+                'x-attribute-two' => 'another value',
+                'foo' => 3,
+                'bar' => false,
+                'foo-bar' => true,
+            ]);
     }
 }

aaa51f45d185e5fb0bde2412b3988aa6aaa3b796
Javier Eguiluz <javier.eguiluz@gmail.com>
2024-10-20 11:13:07 +0200

    Fixes

--- tests/Twig/EasyAdminTwigExtensionTest.php
+++ tests/Twig/EasyAdminTwigExtensionTest.php
@@ -19,7 +19,7 @@ class EasyAdminTwigExtensionTest extends KernelTestCase
     public function testRepresentAsString($value, $expectedValue, bool $assertRegex = false, string|callable|null $toStringMethod = null): void
     {
         $translator = $this->getMockBuilder(TranslatorInterface::class)->disableOriginalConstructor()->getMock();
-        $translator->method('trans')->willReturnCallback(fn($value) => '*'.$value);
+        $translator->method('trans')->willReturnCallback(fn ($value) => '*'.$value);
 
         $extension = new EasyAdminTwigExtension(
             $this->getMockBuilder(ServiceLocator::class)->disableOriginalConstructor()->getMock(),
@@ -63,13 +63,33 @@ class EasyAdminTwigExtensionTest extends KernelTestCase
         yield [true, 'true'];
         yield [false, 'false'];
         yield [[1, 2, 3], 'Array (3 items)'];
-        yield [new class implements TranslatableInterface { public function trans(TranslatorInterface $translator, string $locale = null): string { return $translator->trans('some value'); } }, '*some value'];
+        yield [new class implements TranslatableInterface {
+            public function trans(TranslatorInterface $translator, ?string $locale = null): string
+            {
+                return $translator->trans('some value');
+            }
+        }, '*some value'];
         yield [new class {}, '/class@anonymous.*/', true];
-        yield [new class { public function __toString() { return 'foo bar'; }}, 'foo bar'];
-        yield [new class { public function getId() { return 1234; }}, '/class@anonymous.* #1234/', true];
+        yield [new class {
+            public function __toString()
+            {
+                return 'foo bar';
+            }
+        }, 'foo bar'];
+        yield [new class {
+            public function getId()
+            {
+                return 1234;
+            }
+        }, '/class@anonymous.* #1234/', true];
 
-        yield ['foo', 'foo bar', false, fn($value) => $value.' bar'];
-        yield ['foo', '*foo bar', false, fn($value, $translator) => $translator->trans($value.' bar')];
-        yield [new class () { public function someMethod() { return 'foo'; }}, 'foo', false, 'someMethod'];
+        yield ['foo', 'foo bar', false, fn ($value) => $value.' bar'];
+        yield [new class {
+            public function someMethod()
+            {
+                return 'foo';
+            }
+        }, 'foo', false, 'someMethod'];
+        yield ['foo', '*foo bar', false, fn ($value, $translator) => $translator->trans($value.' bar')];
     }
 }

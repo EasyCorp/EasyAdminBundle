5960e9d9fa83da86cc75d8f8390f9c2cc7051cbf
chapterjason <chapterjason@users.noreply.github.com>
2025-03-09 19:39:40 +0100

    Fix cs

--- src/Field/Configurator/ChoiceConfigurator.php
+++ src/Field/Configurator/ChoiceConfigurator.php
@@ -139,8 +139,8 @@ final class ChoiceConfigurator implements FieldConfiguratorInterface
                 $selectedValue instanceof \UnitEnum => $selectedValue->name,
                 default => $selectedValue,
             };
-            if (is_object($selectedValue) && $selectedValue instanceof \Stringable) {
-                $selectedValue = (string)$selectedValue;
+            if (\is_object($selectedValue) && $selectedValue instanceof \Stringable) {
+                $selectedValue = (string) $selectedValue;
             }
             if (null !== $selectedLabel = $flippedChoices[$selectedValue] ?? null) {
                 if ($selectedLabel instanceof TranslatableInterface) {

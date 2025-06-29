633d47f4b8768d91fb3a9575638cfa90dd253811
mhbailly <max-henri@max-henri.com>
2025-06-29 12:38:22 -0400

    Fixes based on php-linter results 2

--- src/Filter/NumericFilter.php
+++ src/Filter/NumericFilter.php
@@ -2,8 +2,8 @@
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Filter;
 
-use Doctrine\ORM\QueryBuilder;
 use \Doctrine\DBAL\ArrayParameterType;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Filter\FilterInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;

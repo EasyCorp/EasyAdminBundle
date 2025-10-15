fa123bc540e44cda8275b304b53e410ebf832c33
Tac Tacelosky <tacman@gmail.com>
2025-10-15 14:50:17 -0400

    check for doctine 2

--- tests/AdminRouteTestApplication/config/packages/doctrine.php
+++ tests/AdminRouteTestApplication/config/packages/doctrine.php
@@ -1,14 +1,12 @@
 <?php
-
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container) {
-    $container->extension('doctrine', [
+    $options = [
         'dbal' => [
             'url' => 'sqlite:///:memory:',
         ],
         'orm' => [
-            'auto_generate_proxy_classes' => true,
             'auto_mapping' => true,
             'mappings' => [
                 'AdminRouteTestApplication' => [
@@ -20,5 +18,12 @@ return static function (ContainerConfigurator $container) {
                 ],
             ],
         ],
-    ]);
+    ];
+
+    if (!class_exists(\Doctrine\Common\Annotations\AnnotationReader::class)) {
+        // This is Doctrine 3 (annotations package was removed)
+        $options['orm']['auto_generate_proxy_classes'] = true;
+    }
+
+    $container->extension('doctrine', $options);
 };

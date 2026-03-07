5651ec3213133c1466881b1a0f98cca11571f4ff
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2026-03-07 14:56:59 +0200

    feat: add `renderAsLinkToEntity` option to TextField

--- tests/Unit/Field/TextFieldTest.php
+++ tests/Unit/Field/TextFieldTest.php
@@ -136,6 +136,23 @@ class TextFieldTest extends AbstractFieldTest
         self::assertStringContainsString('&lt;script&gt;', $fieldDto->getFormattedValue());
     }
 
+    public function testRenderAsLinkToEntity(): void
+    {
+        $field = TextField::new('foo');
+        $field->renderAsLinkToEntity();
+        $fieldDto = $this->configure($field);
+
+        self::assertTrue($fieldDto->getCustomOption(TextField::OPTION_RENDER_AS_LINK_TO_ENTITY));
+    }
+
+    public function testRenderAsLinkToEntityDefaultIsFalse(): void
+    {
+        $field = TextField::new('foo');
+        $fieldDto = $this->configure($field);
+
+        self::assertFalse($fieldDto->getCustomOption(TextField::OPTION_RENDER_AS_LINK_TO_ENTITY));
+    }
+
     public function testBackedEnumValue(): void
     {
         // create a backed enum for testing
